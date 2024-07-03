27ef3ae5b396849083b882db982544db338dacc9
tejth <ptejendra91@gmail.com>
2024-07-03 19:57:21 +0530

    add header component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Home from "./components/Home";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
